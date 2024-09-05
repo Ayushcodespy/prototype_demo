423dcb531d1f45c695ccd855fa1471003bbbffc8
Ayush <ayush.jnv54@gmail.com>
2024-09-05 18:54:49 +0530

    -

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.secret_key = "super-secret-key"
 @app.route('/')
 def home():
     if 'user' in session:
-        return render_template("index.html")
+        return render_template("login.html")
 
     else:
         return redirect(url_for('login'))
@@ -14,36 +14,49 @@ def home():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    if 'user' in session:
-        return redirect(url_for('home'))
-
     if request.method == "POST":
         username = request.form['user_id']
         password = request.form['passwd']
         user_type = request.form['user_type']
 
+        print(f"Username: {username}, Password: {password}, User Type: {user_type}")  # Debug line
+
         if user_type == 'administration':
-            if username == 'admin'and password == 'admin':
-                session['user'] = username  # Store user in session
-                return redirect(url_for('home'))
+            if username == 'admin' and password == 'admin':
+                session['user'] = username
+                return redirect(url_for('admin_page'))
             else:
                 return render_template("login.html", error='Incorrect login details!')
 
         if user_type == 'alumini':
-            if username == 'alumini'and password == 'alumini':
-                session['user'] = username  # Store user in session
-                return redirect(url_for('home'))
+            if username == 'alumni' and password == 'alumni':
+                session['user'] = username
+                return redirect(url_for('alumni_page'))
             else:
                 return render_template("login.html", error='Incorrect login details!')
 
         if user_type == 'student':
-            if username == 'student'and password == 'student':
-                session['user'] = username  # Store user in session
-                return redirect(url_for('home'))
+            if username == 'student' and password == 'student':
+                session['user'] = username
+                return redirect(url_for('student_page'))
             else:
                 return render_template("login.html", error='Incorrect login details!')
 
     return render_template('login.html')
+
+
+
+@app.route("/admin_page")
+def admin_page():
+    return render_template('admin_page.html')
+
+@app.route("/alumni_page")
+def alumni_page():
+    return render_template('alumni_page.html')
+
+@app.route("/student_page")
+def student_page():
+    return render_template('student_page.html')
 
 
 @app.route("/logout", methods=['GET','POST'])
