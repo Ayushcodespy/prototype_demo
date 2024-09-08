cc270131fceb35020c7c051c533f7d9c0e1f8db7
Ayush <ayush.jnv54@gmail.com>
2024-09-09 04:23:54 +0530

    __

--- static/script/home.js
+++ static/script/home.js
@@ -29,29 +29,3 @@ function closeAllSubMenus(){
     ul.previousElementSibling.classList.remove('rotate')
   })
 }
-
-
-
-// swaper added by ayush
-document.addEventListener('DOMContentLoaded', () => {
-  const track = document.querySelector('.carousel-track');
-  const slides = Array.from(track.children);
-  const slideWidth = slides[0].getBoundingClientRect().width;
-
-  let currentIndex = 0;
-
-  const moveToSlide = (track, currentIndex) => {
-      track.style.transform = 'translateX(-' + slideWidth * currentIndex + 'px)';
-  };
-
-  const autoSlide = () => {
-      if (currentIndex < slides.length - 1) {
-          currentIndex++;
-      } else {
-          currentIndex = 0;
-      }
-      moveToSlide(track, currentIndex);
-  };
-
-  setInterval(autoSlide, 3000); // Change slide every 3 seconds
-});
